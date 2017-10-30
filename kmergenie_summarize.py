4a08d6cc97d9f2392b5fc33dd20e12ac5512ba30
emreerhan <eerhan@gphost03.bcgsc.ca>
2017-10-30 10:29:36 -0700

    Fixed bug in kmergenie_summarize.py where same kmer size was being added multiple times. Added kmergenie_summary.tsv. Note: All assemblies are now done, and data is being generated. More data to come.

--- kmergenie_summarize.py
+++ kmergenie_summarize.py
@@ -7,18 +7,18 @@ def main():
     quast_report = sys.argv[1]
     assembly_data = pd.read_csv(quast_report, sep='\t')
     reads = assembly_data['Assembly'].apply(lambda x: x.strip('-contigs'))
-    
+    kg_kmers = [] 
     root = '/projects/btl/eerhan/simulated_data/ecoli'
     for read in reads:
         read_dir = read[4:]
         # k = read[:3]
         kmergenie_path = "{}/{}/kmergenie/kg_kmer.txt".format(root, read_dir)
-        kg_kmers = []
+        print(kmergenie_path)
         with open(kmergenie_path) as kgenie_file:
             optimal_k = kgenie_file.readline().rstrip().split(': ')[1]
             kg_kmers.append(int(optimal_k))
             kgenie_file.close()
-        kmergenie_data = pd.DataFrame({"kmergenie": pd.Series(kg_kmers, index=reads)}, index=reads)
+    kmergenie_data = pd.DataFrame({"kmergenie": pd.Series(kg_kmers, index=reads)}, index=reads)
     kmergenie_data.to_csv('kmergenie_summary.tsv', sep='\t')
 
 
